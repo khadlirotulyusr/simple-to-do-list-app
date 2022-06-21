c6a1cdab65cf4e2855f810babeeaffe5f82a19a4
DESKTOP-DTVR69N\dell <yusroayyun26@gmail.com>
2022-06-21 23:04:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SubmitForm from "./components/SubmitForm/SubmitForm";
+import TaskList from "./components/Task/TaskList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { tasks : []}
+  }
+
+  addTask = (task) => {
+    this.setState((prevState) => {
+      return {
+        tasks: prevState.tasks.concat(task)
+      };
+    });
+  }
+
+  removeTask = (id) => {
+    const tasks = this.state.tasks.filter(el => (el.id !== id));
+    this.setState({tasks:tasks});
+  }
+  
+
+  render(){
+    return (
+      <div className="App">
+      <h3 className="App-header">Simple To-Do-List App</h3>
+      <div className="App">
+      <SubmitForm addTask={this.addTask}/>
+      <TaskList tasks={this.state.tasks} removeTask={this.removeTask}/>
+      <h3 className="App-footer">	&copy;khadlirotulyusr.2022</h3>
+
     </div>
-  );
+    </div>
+
+    );
+  }
 }
+
 
 export default App;
